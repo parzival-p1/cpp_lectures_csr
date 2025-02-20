7d4b8bc6c1050bc2c436ec79503097d53bd80ea8
Francisco <francj.hdez@gmail.com>
2025-02-19 18:00:57 -0600

    leetcode 168: Excel Sheet Column - SOLVED

--- leetcode/168_excel_shet_column_title/main.c
+++ leetcode/168_excel_shet_column_title/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
-char* convertToTitle(int columnNumber)
+char* convertToTitle1(int columnNumber)
 {
-    int numChars = (int)floor(log(columnNumber) / log(26)) + 1;
+    int numChars = (columnNumber == 1) ? 1 : (int)floor(log(columnNumber - 1) / log(26)) + 1;
     char* column = (char*)malloc(sizeof(char) * numChars + 1);
     int chunk = columnNumber;
+
     // Como tratar todas las letras como si fueran la ultima, que todas apliquen lo mismo
     // que le tiene que pasar a chunk? para que la pos donde estoy se comporte como el ultimo char, si lo dividio o mod de 26 que da??
 
@@ -15,39 +17,42 @@ char* convertToTitle(int columnNumber)
     // cuanto debe vale chunk para sacar la 1a A ?
     // cuanto debe valer chunk para sacar 2a A ?
     // cuanto debeb valer chunk para sacar la B?
-    for (int i = 0; i < numChars; i++)
+    printf("numChars = %i\n", numChars);
+    for (int i = numChars - 1; i >= 0; i--)
     {
-        column[i] = 64 + chunk % 26;
+        column[i] = 64 + (chunk - 1) % 26 + 1;
+        chunk = (chunk - 1) / 26;
     }
 
     column[numChars] = '\0';
     return column;
 }
 
-/*
+char* convertToTitle(int columnNumber)
+{
+    char *column = (char *)malloc(sizeof(char) * 20);
+    char *index = &column[19];
+    char *result;
+    column[19] = '\0';
+    int chunk = columnNumber;
 
-func convertToTitle(columnNumber):
-    resultado = ""  // String vacío para construir el resultado
+    while(chunk > 0)
+    {
+        index--;
+        *index = 64 + (chunk - 1) % 26 + 1;
+        chunk = (chunk - 1) / 26;
+    }
+    result = (char *)malloc(sizeof(char) * strlen(index));
+    strcpy(result, index);
+    free(column);
+    return result;
+}
 
-    mientras columnNumber > 0:
-        columnNumber = columnNumber - 1  // Ajuste por la base 1 en Excel
-        residuo = columnNumber % 26  // Obtener la letra correspondiente
-        letra = convertir residuo a carácter (residuo + 'A')  // 'A' = 0
-        resultado = letra + resultado  // Agregar al inicio del resultado
-        columnNumber = columnNumber % 26  // Reducir el número
-
-    retornar resultado
-
- Z A Z
- Z B A
- T Z Z
- U A A
-*/
 int main()
 {
     char* result;
-    int column = 1;
-    printf("%i\n", (26 -  1) % 26);
+    int column = 18279;
+   //printf("%i\n", (26 -  1) % 26);
 
     result = convertToTitle(column);
     printf("\n%s\n", result);
